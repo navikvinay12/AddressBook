af4a12bdec2c89124aa419ab1fa9b6b80ad5e41c
Vinay <navik.vinay12@gmail.com>
2022-11-22 11:23:05 +0530

    added welcome msg

---
+++ AddressBook.cs
@@ -0,0 +1,14 @@
+﻿namespace AddressBookApp
+{
+    internal class AddressBook
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Address Book Program!\n");
+        }
+        static void Main(string[] args)
+        {
+            AddressBook.WelcomeMessage();
+        }
+    }
+}
